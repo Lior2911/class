2ccccbe094ead668af31a28f9a4d49b2abab7153
lior2911 <aytegev1@gmail.com>
2022-06-19 22:48:36 +0300

    add

---
+++ main.js
@@ -0,0 +1,31 @@
+// class Student {
+//   constructor(firstName,grade,Id){
+//     this.firstName = firstName;
+//     this.grade = grade;
+//     this.Id = Id;
+//   }
+
+// }
+// const newStudent = new Student("lior",12,"305207573");
+// const oldStudent = new Student("daniel",12,"123456789");
+// console.log(newStudent);
+// console.log(oldStudent);
+
+// class UserDetails{
+//   constructor(firstName,lastName,age,mail){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.age = age;
+//     this.mail = mail;
+//   }
+// }
+// const firstUser = new UserDetails("lior","aytegev",31,"aytegev1@gmail.com");
+// const sectUser = new UserDetails("daniel","malada",24,"malada1@gmail.com");
+// const thirdtUser = new UserDetails("naruto","uzumaki","hokage@gmail.com");
+// const detailsLine = document.createElement("h3");
+// document.body.append(detailsLine)
+// const usersArray = [];
+// for ( const key in firstUser){
+//  detailsLine.innerText += `${key}:${firstUser[key]}\n`
+// }
+
